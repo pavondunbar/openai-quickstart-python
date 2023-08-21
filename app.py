c4389cd7c233e6b35883516f00a5087de3145445
root <root@info@theto.co>
2023-08-21 13:41:13 -0700

    Initial Deployment 08-21-2023

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 import os
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
+def generate_prompt(question):
+    return f"You are a helpful assistant that answers questions.\n\nQ: {question}\nA:"
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        question = request.form["question"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question}
+            ],
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        answer = response.choices[0].message["content"]
+        return render_template("index.html", question=question, answer=answer)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    return render_template("index.html")
 
+# Uncomment the below if you want to use a development server to test your application.
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+# Uncomment the below if you want to use Waitress as your production server to test your application.
+
+# if __name__ == '__main__':
+#     # Run the application using Waitress
+#     from waitress import serve
+#     serve(app, host='0.0.0.0', port=8000)
+
+# Gunicorn below is the default production server. Simply run `gunicorn app:application` to fire off the Gunicorn prod server.
+
+# Export the Flask application object as 'application'
+application = app
